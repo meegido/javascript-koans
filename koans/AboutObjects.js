d96417489f63f697ad4a9cfb010775fd27a916b3
Mercedes Egido <meegido@gmail.com>
2015-03-31 13:13:48 +0200

    Objects, properties, function properties act like methods, this, in keyword and prototype

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -8,12 +8,12 @@ describe("About Objects", function () {
     });
 
     it("should confirm objects are collections of properties", function () {
-      expect(megalomaniac.mastermind).toBe(FILL_ME_IN);
+      expect(megalomaniac.mastermind).toBe("Joker");//Accedemos a la propiedad de la clave mastermind del objeto
     });
 
     it("should confirm that properties are case sensitive", function () {
-      expect(megalomaniac.henchwoman).toBe(FILL_ME_IN);
-      expect(megalomaniac.henchWoman).toBe(FILL_ME_IN);
+      expect(megalomaniac.henchwoman).toBe("Harley");//Accedemos a la propiedad de la clave henchwoman
+      expect(megalomaniac.henchWoman).toBe(undefined);//No accedemos porque la clave está escrita con Woman en mayúscula y así no está definida.
     });
   });
 
@@ -24,12 +24,15 @@ describe("About Objects", function () {
       henchman: "Pinky",
       battleCry: function (noOfBrains) {
         return "They are " + this.henchman + " and the" +
-          Array(noOfBrains + 1).join(" " + this.mastermind);
+          Array(noOfBrains + 1).join(" " + this.mastermind);//The join() method joins the elements of an array into a string, and returns the string.
       }
     };
 
-    var battleCry = megalomaniac.battleCry(4);
-    expect(FILL_ME_IN).toMatch(battleCry);
+    var battleCry = megalomaniac.battleCry(4);//une 4 veces el texto que tiene como argumento (" " + this.mastermind)
+    var expected = "They are Pinky and the Brain Brain Brain Brain"
+    /*La propiedad-función battleCry del objeto mastermind, accede a la la propiedad del objeto
+    con .this, entre strings, crea un array (4 + 1) que une con join a la propiedad mastermind y juega con los espacios(hacky).*/
+    expect(expected).toMatch(battleCry);
   });
 
   it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
@@ -44,8 +47,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2015);//le da argumento a currentYear para luego
+    expect(megalomaniac.calculateAge()).toBe(45);//hacer el cálculo de la función/método calculateYear que tiene el .this
   });
 
   describe("'in' keyword", function () {
@@ -60,34 +63,34 @@ describe("About Objects", function () {
 
     it("should have the bomb", function () {
 
-      var hasBomb = "theBomb" in megalomaniac;
+      var hasBomb = "theBomb" in megalomaniac;//The in operator returns true if the specified property is in the specified object.
 
-      expect(hasBomb).toBe(FILL_ME_IN);
+      expect(hasBomb).toBe(true);
     });
 
     it("should not have the detonator however", function () {
 
       var hasDetonator = "theDetonator" in megalomaniac;
 
-      expect(hasDetonator).toBe(FILL_ME_IN);
+      expect(hasDetonator).toBe(false);
     });
   });
 
   it("should know that properties can be added and deleted", function () {
     var megalomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
 
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(false);
 
     megalomaniac.secretary = "Agent Smith";
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(true);
 
     delete megalomaniac.henchman;
-    expect("henchman" in megalomaniac).toBe(FILL_ME_IN);
+    expect("henchman" in megalomaniac).toBe(false);//If you delete a property with the delete operator, the in operator returns false for that property.
   });
 
 
-  it("should use prototype to add to all objects", function () {
-      function Circle(radius)
+  it("should use prototype to add to all objects", function () {//prototype añade ese método a todos los objetos de esa clase
+      function Circle(radius)//es clase cuando la creo con function. Si no hay clase, new Object coje la clase objeto por defecto en js
       {
         this.radius = radius;
       }
@@ -96,14 +99,14 @@ describe("About Objects", function () {
       var colouredCircle = new Circle(5);
       colouredCircle.colour = "red";
 
-      expect(simpleCircle.colour).toBe(FILL_ME_IN);
-      expect(colouredCircle.colour).toBe(FILL_ME_IN);
+      expect(simpleCircle.colour).toBe(undefined);//porque SimpleCircle no tiene esa propiedad
+      expect(colouredCircle.colour).toBe("red");
 
       Circle.prototype.describe = function () {
         return "This circle has a radius of: " + this.radius;
       };
 
-      expect(simpleCircle.describe()).toBe(FILL_ME_IN);
-      expect(colouredCircle.describe()).toBe(FILL_ME_IN);
+      expect(simpleCircle.describe()).toBe("This circle has a radius of: 10");
+      expect(colouredCircle.describe()).toBe("This circle has a radius of: 5");
   });
 });
